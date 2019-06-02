2956e78a6cdc6b609e924aeb9885d6dea3051aaf
thewchan <thewchan@gmail.com>
2019-06-01 17:27:44 -0700

    Pressing Q to Quit

--- alien_invasion.py
+++ alien_invasion.py
@@ -45,6 +45,8 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             # Move the ship to the left
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
