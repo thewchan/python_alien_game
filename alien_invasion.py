766b6d2926b74f81bc1d7ad5380f473e29661625
thewchan <thewchan@gmail.com>
2019-06-01 19:47:29 -0700

    Refactoring: Creating the _update_bullets() Method

--- alien_invasion.py
+++ alien_invasion.py
@@ -27,15 +27,8 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
-
-            # Get rid of bullets that have disappeared.
-            for bullet in self.bullets.copy():
-                if bullet.rect.bottom <= 0:
-                    self.bullets.remove(bullet)
-            # This line of code below checks with bullet removal works.
-            # print(len(self.bullets))
 
     def _check_events(self):
         """Respond to keypresses and mouse events."""
@@ -71,6 +64,18 @@ class AlienInvasion:
         if len(self.bullets) < self.settings.bullets_allowed:
             new_bullet = Bullet(self)
             self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+        # This line of code below checks with bullet removal works.
+        # print(len(self.bullets))
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
