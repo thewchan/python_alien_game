fbae84613fb292643752cee26dca8f5dd05b760f
thewchan <thewchan@gmail.com>
2019-06-03 08:39:02 -0700

    Moving the Aliens Right

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
@@ -80,6 +81,10 @@ class AlienInvasion:
                 self.bullets.remove(bullet)
         # This line of code below checks with bullet removal works.
         # print(len(self.bullets))
+
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
